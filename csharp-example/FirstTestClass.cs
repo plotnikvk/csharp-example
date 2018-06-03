e2d751f021fe4c04b4abfebdcd4061754f98f8b9
Владимир Плотников <plotnikov.vova@mail.ru>
2018-06-03 20:35:57 +0300

    Commit1

---
+++ csharp-example/FirstTestClass.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using OpenQA.Selenium.Support.Events;
+using System.Collections.Generic;
+
+namespace csharp_example
+{
+    [TestFixture]
+    class FirstTestClass
+    {
+        private EventFiringWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new EventFiringWebDriver(new ChromeDriver("C:\\Users\\plotn\\Documents" +
+                "\\Visual Studio 2017\\Projects\\csharp-example\\csharp-example\\drv\\"));
+            driver.FindingElement += (sender, e) => Console.WriteLine(e.FindMethod);
+            driver.FindElementCompleted += (sender, e) => Console.WriteLine(e.FindMethod + " found");
+            driver.ExceptionThrown += (sender, e) => Console.WriteLine(e.ThrownException);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.google.com/";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Tab);
+            IWebElement searchButton = wait.Until(d => d.FindElement(By.Name("btnK")));
+            
+            //IList<IWebElement> links =  driver.FindElements(By.XPath("//h3/a"));
+            //links[0].Click();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ csharp-example/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("csharp-example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("csharp-example")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("debe92ec-ce4b-490a-90b2-237ca59e64a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
